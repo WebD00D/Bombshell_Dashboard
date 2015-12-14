7264b1a953fa849933b3ec37e7d691237211ec21
Christian <Bryant>
2015-12-14 18:37:30 -0500

    further adding of services

--- js/BombshellDashboard.js
+++ js/BombshellDashboard.js
@@ -189,10 +189,145 @@ $(document).ready(function(){
   $(".addService").click(function(e){
     e.preventDefault();
     var theServiceType = $(this).attr("data-service");
-    alert(theServiceType);
+    $("#AddServiceName").text(theServiceType);
+    //load Services by type.
+    var Service = Parse.Object.extend("Services")
+    var query = new Parse.Query(Service);
+      query.equalTo("ServiceType",theServiceType);
+      query.find({
+        success: function(results) {
+          $("#dlServiceName").empty();
+          // Do something with the returned Parse.Object values
+          for (var i = 0; i < results.length; i++) {
+            var object = results[i];
+              var listItem = "<option value='"+ object.id +"'>"+ object.get('Service') +"</option>"
+              $(listItem).appendTo("#dlServiceName");
+
+          }
+          var disabledOption = "<option value='zzz' disabled selected>Select service </option>"
+          $(disabledOption).prependTo("#dlServiceName");
+        },
+        error: function(error) {
+          alert("Error: " + error.code + " " + error.message);
+        }
+      });
+    $("#ServiceAddWindow").openModal();
+
   })
 
+  $("#dlServiceName").change(function(){
+    var selectedService = $(this).val();
+    $("#btnAddServicetoLocation").attr("data-selectedService",selectedService);
+  })
 
+  $("#btnAddServicetoLocation").click(function(){
+    var selectedServiceOption = $("#dlServiceName option:selected").val();
+    if (selectedServiceOption === 'zzz'){
+      $("#btnAddServicetoLocation").removeClass("pink").addClass("red").text("Invalid Selection");
+      return;
+    } else {
+      $("#btnAddServicetoLocation").removeClass("red").addClass("pink").text("Save");
+      var selectedService = $(this).attr("data-selectedService");
+      //check to see if this service has already been added at this location.
+      var selectedLocation = $("#BombshellLocation").attr("data-location");
+
+      // POINTER
+      var Services = Parse.Object.extend("Services");
+      var services = new Services();
+      services.id = selectedService;
+
+      var checkService = Parse.Object.extend("ServicesOffered")
+      var checkServiceQuery = new Parse.Query(checkService);
+          checkServiceQuery.equalTo("Location",selectedLocation);
+          checkServiceQuery.equalTo("ServiceID",services);
+          checkServiceQuery.find({
+            success: function(results) {
+            if (results.length != 0){
+              swal({
+                confirmButtonColor: "#009688",
+                title: "Duplicate found!",
+                text: "The selected service is already on the menu at this location.",
+                type: "error",
+                showCancelButton:false,
+                confirmButtonColor:"#808080",
+                confirmButtonText:"OK",
+                closeOnConfirm: true
+              })
+            } else {
+
+              //check Price
+              if ( $.trim($("#txtAddServicePrice").val()).length == 0 ){
+                swal({
+                  confirmButtonColor: "#009688",
+                  title: "Invalid Price!",
+                  text: "Please make sure price is of numerical value. ",
+                  type: "error",
+                  showCancelButton:false,
+                  confirmButtonColor:"#808080",
+                  confirmButtonText:"OK",
+                  closeOnConfirm: true
+                })
+                return;
+
+              } else {
+
+                //add this service to the service menu.
+                var ServicesOffered = Parse.Object.extend("ServicesOffered");
+                var servicesoffered = new ServicesOffered();
+
+                servicesoffered.set("ServiceID", services);
+                servicesoffered.set("Location", selectedLocation);
+                servicesoffered.set("Price", $("#txtAddServicePrice").val());
+
+                servicesoffered.save(null, {
+                  success: function(servicesoffered) {
+                    // Execute any logic that should take place after the object is saved.
+                    swal({
+                      confirmButtonColor: "#009688",
+                      title: "Save Successful!",
+                      text: "Service successfully added.",
+                      type: "success",
+                      showCancelButton:false,
+                      confirmButtonColor:"#e91e63",
+                      confirmButtonText:"Cool",
+                      closeOnConfirm: false
+                    },
+                    function(){
+                      window.location.reload();
+                    });
+                  },
+                  error: function(servicesoffered, error) {
+                    // Execute any logic that should take place if the save fails.
+                    // error is a Parse.Error with an error code and message.
+                      swal("Uh oh!", error.message, "error")
+                  }
+                });
+              }
+            }
+            },
+            error: function(error) {
+              alert("Error: " + error.code + " " + error.message);
+            }
+          });
+    }
+
+  }) //btnAddServiceLocation end click function
+
+
+  $("#txtAddServicePrice").keydown(function(e){
+    if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+              // Allow: Ctrl+A, Command+A
+             (e.keyCode == 65 && ( e.ctrlKey === true || e.metaKey === true ) ) ||
+              // Allow: home, end, left, right, down, up
+             (e.keyCode >= 35 && e.keyCode <= 40)) {
+                  // let it happen, don't do anything
+                  return;
+         }
+         // Ensure that it is a number and stop the keypress
+         if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+             e.preventDefault();
+         }
+  })
 
 
 
